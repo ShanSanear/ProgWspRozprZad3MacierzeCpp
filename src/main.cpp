039e252814bd1842fd7aeff638ea78e611692f04
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-03-26 21:03:37 +0100

    Added saving matrixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <iomanip>
+#include <iterator>
 
 using matrix = std::vector<std::vector<double>>;
 
-matrix parseCSV(const std::string& input_csv_file) {
+matrix parseCSV(const std::string &input_csv_file) {
     std::ifstream data(input_csv_file);
     std::string line;
+    // Skipping required dimension lines
+    std::getline(data, line);
+    std::getline(data, line);
     matrix parsedCsv;
     while (std::getline(data, line)) {
         std::vector<double> parsedRow;
@@ -22,28 +27,44 @@ matrix parseCSV(const std::string& input_csv_file) {
     return parsedCsv;
 };
 
-matrix multiply_matrixes(const matrix& matrix_a, const matrix& matrix_b) {
+matrix multiply_matrixes(const matrix &matrix_a, const matrix &matrix_b) {
     matrix output_matrix(matrix_a.size(), std::vector<double>(matrix_b.at(0).size()));
-    for(std::size_t row = 0; row < output_matrix.size(); ++row) {
-        for(std::size_t col = 0; col < output_matrix.at(0).size(); ++col) {
-            for(std::size_t inner = 0; inner < matrix_b.size(); ++inner) {
+    for (std::size_t row = 0; row < output_matrix.size(); row++) {
+        for (std::size_t col = 0; col < output_matrix.at(0).size(); col++) {
+            for (std::size_t inner = 0; inner < matrix_b.size(); inner++) {
                 output_matrix.at(row).at(col) += matrix_a.at(row).at(inner) * matrix_b.at(inner).at(col);
             }
         }
     }
     return output_matrix;
+}
 
+void save_matrix (const matrix& matrix_to_save) {
+    std::ofstream output("file.csv");
+
+    output << matrix_to_save.size() << std::endl;
+    output << matrix_to_save.at(0).size() << std::endl;
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(5);
+    for (auto row : matrix_to_save) {
+        oss.str(std::string());
+        std::copy(row.begin(), row.end(), std::ostream_iterator<double>(oss, ";"));
+        output << oss.str() << std::endl;
+    }
 }
 
 int main() {
     matrix matrix_a = parseCSV(R"(M:\C++\Projects\ProgWspRozprZad3MacierzeCpp\matrix_a.csv)");
     matrix matrix_b = parseCSV(R"(M:\C++\Projects\ProgWspRozprZad3MacierzeCpp\matrix_b.csv)");
     auto output_matrix = multiply_matrixes(matrix_a, matrix_b);
+    std::cout << std::fixed;
+    std::cout << std::setprecision(5);
     for (std::vector<double> &row : output_matrix) {
         for (double &val : row) {
-            std::cout << val << std::endl;
+            std::cout << val << ";";
         }
-        std::cout << "Next line" << std::endl;
+        std::cout << std::endl;
     }
+    save_matrix(output_matrix);
     return 0;
 }
