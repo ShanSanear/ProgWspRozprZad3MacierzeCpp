923f7084b7a120e1e51e0934fa81cfae7aff6ed5
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-03-26 21:04:07 +0100

    Changes reflecing requirements by the task (dimensions in first two lines).

--- scripts/generate_random_matrixes.py
+++ scripts/generate_random_matrixes.py
@@ -3,8 +3,8 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', help="N dimension of the output matrix", type=int)
-parser.add_argument('-m', help="M dimension of the output matrix", type=int)
+parser.add_argument('-n', help="N dimension of the output matrix", type=int, required=True)
+parser.add_argument('-m', help="M dimension of the output matrix", type=int, required=True)
 parser.add_argument('--output', help="Output file path where the matrix will be placed", default="out.csv")
 parser.add_argument('--lower', help='Low limit of the values possible in matrix', type=float, default=0.0)
 parser.add_argument('--higher', help='High limit of the values possible in matrix', type=float, default=150.0)
@@ -19,4 +19,11 @@ output = args.output
 int_random = np.random.randint(lower, higher, (n, m))
 double_random = np.random.rand(n, m)
 finished_matrix = int_random * double_random
-finished_matrix.tofile(output, sep=';')
+np.savetxt(output, finished_matrix, fmt='%.5f', delimiter=';')
+
+with open(output, 'r+') as file:
+    content = file.read()
+    file.seek(0, 0)
+    file.write(f"{finished_matrix.shape[0]}\n")
+    file.write(f"{finished_matrix.shape[1]}\n")
+    file.write(content)
--- scripts/multiply_matrixes.py
+++ scripts/multiply_matrixes.py
@@ -7,7 +7,13 @@ parser.add_argument("output", nargs=1)
 args = parser.parse_args()
 matrixes = args.matrixes
 output = args.output[0]
-matrix_a = np.genfromtxt(matrixes[0], delimiter=';')
-matrix_b = np.genfromtxt(matrixes[1], delimiter=';')
+matrix_a = np.genfromtxt(matrixes[0], delimiter=';', skip_header=2)
+matrix_b = np.genfromtxt(matrixes[1], delimiter=';', skip_header=2)
 out = np.dot(matrix_a, matrix_b)
 np.savetxt(output, out, fmt='%.5f', delimiter=';')
+with open(output, 'r+') as file:
+    content = file.read()
+    file.seek(0, 0)
+    file.write(f"{out.shape[0]}\n")
+    file.write(f"{out.shape[1]}\n")
+    file.write(content)
